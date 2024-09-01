78fb54a812873de6cf8a86ee33369e7a6c36344e
skgmain1 <gamerlauncher3399@gmail.com>
2024-09-01 15:54:34 +0300

    Update by skgmain.lua

--- by skgmain.lua
+++ by skgmain.lua
@@ -80,7 +80,7 @@ local RayfieldLibrary = {
 			ElementBackground = Color3.fromRGB(198, 198, 198), -- Фон элементов (светло-серый)
 			ElementBackgroundHover = Color3.fromRGB(230, 230, 230), -- Фон при наведении (очень светло-серый)
 			SecondaryElementBackground = Color3.fromRGB(136, 136, 136), -- Фон для второстепенных элементов (серый)
-			ElementStroke = Color3.fromRGB(186, 85, 211), -- Обводка элементов (нежный фиолетовый)
+			ElementStroke = Color3.fromRGB(200, 160, 255), -- Обводка элементов (нежный пастельный фиолетовый)
 			SecondaryElementStroke = Color3.fromRGB(40, 40, 40), -- Обводка второстепенных элементов (тёмно-серый)
 
 			SliderBackground = Color3.fromRGB(144, 0, 255), -- Фон слайдера (фиолетовый)
